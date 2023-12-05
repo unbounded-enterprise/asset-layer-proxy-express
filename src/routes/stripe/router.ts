fdf7335549afea30f2755bd19945c661f60763e0
Nadrojiskool <jjbhaveagoodtime@yahoo.com>
2023-12-05 11:32:05 -0800

    add paymentIntent

--- src/routes/stripe/router.ts
+++ src/routes/stripe/router.ts
@@ -4,7 +4,7 @@ import { createPaymentIntent, paymentIntentWebhook } from './handlers';
 
 const stripeRouter: Router = Router();
 
-stripeRouter.post('/createPaymentIntent', bodyParser.raw({ type: 'application/json' }), createPaymentIntent);
+stripeRouter.post('/createPaymentIntent', createPaymentIntent);
 stripeRouter.post('/paymentIntentWebhook', bodyParser.raw({ type: 'application/json' }), paymentIntentWebhook);
 
 export default stripeRouter;
