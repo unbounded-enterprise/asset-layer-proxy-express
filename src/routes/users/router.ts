40780285ae8854dc37c4d886862764bdbf826a9b
Nadrojiskool <jjbhaveagoodtime@yahoo.com>
2024-02-01 18:59:45 -0800

    add user/collections route

--- src/routes/users/router.ts
+++ src/routes/users/router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getUser, registerUser } from './handlers';
+import { getUser, getUserCollections, registerUser } from './handlers';
 
 const usersRouter: Router = Router();
 
 usersRouter.get('/info', getUser);
+usersRouter.get('/collections', getUserCollections);
 
 usersRouter.post('/register', registerUser);
 
