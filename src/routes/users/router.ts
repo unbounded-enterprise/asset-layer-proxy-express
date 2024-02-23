859992229e1ebae14d6848d50b3304ecaf6d3a03
Nadrojiskool <jjbhaveagoodtime@yahoo.com>
2024-02-23 12:23:18 -0800

    enable route

--- src/routes/users/router.ts
+++ src/routes/users/router.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { getUser, getUserCollections } from './handlers';
+import { getUser, getUserCollections, registerUser } from './handlers';
 
 const usersRouter: Router = Router();
 
 usersRouter.get('/info', getUser);
 usersRouter.get('/collections', getUserCollections);
 
-// usersRouter.post('/register', registerUser);
+usersRouter.post('/register', registerUser);
 
 export default usersRouter;
